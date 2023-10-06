3916a99e4286aa836feb40e7089b2d419080882b
Gabriel <gabrielmarkolinosilva14@gmail.com>
2023-10-06 09:32:53 -0300

    chore: criação da pag de cadastro de batalhas

--- src/routes/CadBatle/index.jsx
+++ src/routes/CadBatle/index.jsx
@@ -1,13 +1,54 @@
-import React from "react";
-import "./style.css"
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { baseURL } from "../../assets/axios/config";
 
-const CadBatle = () =>{
+import "./style.css"
+
+const CadBatle = () => {
     const { modalidade } = useParams()
+
+    const [competidores, setCompetidores] = useState([])
+
+    useEffect(() => {
+        axios.get(`${baseURL}/competidor`).then((res) => setCompetidores(res.data))
+    }, [])
+
+    console.log(competidores)
 
     return (
         <>
-            {modalidade}
+            <section id="arena">
+                
+            </section>
+            <section id="continer-cards-batle">
+                {competidores.map((item) => {
+                    if (item.modalidade === modalidade) {
+                        return (
+                            <section className="min-card" key={item._id}>
+                                <section className="header">
+                                    <p>{item.ranking}</p>
+                                    <h5>{item.nomeCompetidor}</h5>
+                                </section>
+
+                                <section className="competidor">
+                                    <div style={{ backgroundImage: `url(${item.linkGif})`, backgroundSize: "contain", width: "100%", height: "100%", backgroundRepeat: "no-repeat", backgroundPosition: "center" }} > </div>
+                                </section>
+                                <section className="robo">
+                                    <img src={item.linkRobo} />
+                                </section>
+                                <section className="body">
+                                    <section className="content">
+                                        <p><b>Equipe:</b> <span>{item.equipe}</span></p>
+                                        <p><b>Modalidade:</b> <span>{item.modalidade}</span></p>
+                                        <p><b>Instituição:</b> <span>{item.instituicao}</span></p>
+                                    </section>
+                                </section>
+                            </section>
+                        )
+                    }
+                })}
+            </section>
         </>
     )
 }
