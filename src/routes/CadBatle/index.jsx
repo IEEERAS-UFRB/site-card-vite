c1710c08a509c19b0d6bd28185ca19a47adf928e
Gabriel <gabrielmarkolinosilva14@gmail.com>
2023-10-06 10:13:55 -0300

    chore: as batalhas podem começar agr

--- src/routes/CadBatle/index.jsx
+++ src/routes/CadBatle/index.jsx
@@ -10,22 +10,89 @@ const CadBatle = () => {
 
     const [competidores, setCompetidores] = useState([])
 
+    const [batalha, setBatalha] = useState([])
+
     useEffect(() => {
         axios.get(`${baseURL}/competidor`).then((res) => setCompetidores(res.data))
     }, [])
+
+    const fight = (item) => {
+
+        const newBatle = [
+            ...batalha,
+            item
+
+        ]
+
+        setBatalha(newBatle)
+
+        console.log(batalha)
+    }
 
     console.log(competidores)
 
     return (
         <>
             <section id="arena">
-                
+                {(batalha.length < 3 && modalidade !== "Follow Line") ? ( //chamar cada compoente modalidade pra ficar uma melhor visualização, visto que serão itens diferentes
+                    batalha.map((item) => {
+                        return (
+                            <section className="min-card" key={item._id}>
+                                <section className="header">
+                                    <p>{item.ranking}</p>
+                                    <h5>{item.nomeCompetidor}</h5>
+                                </section>
+
+                                <section className="competidor">
+                                    <div style={{ backgroundImage: `url(${item.linkGif})`, backgroundSize: "contain", width: "100%", height: "100%", backgroundRepeat: "no-repeat", backgroundPosition: "center" }} > </div>
+                                </section>
+                                <section className="robo">
+                                    <img src={item.linkRobo} />
+                                </section>
+                                <section className="body">
+                                    <section className="content">
+                                        <p><b>Equipe:</b> <span>{item.equipe}</span></p>
+                                        <p><b>Modalidade:</b> <span>{item.modalidade}</span></p>
+                                        <p><b>Instituição:</b> <span>{item.instituicao}</span></p>
+                                    </section>
+                                </section>
+                            </section>
+                        )
+                    })
+                ) : ((modalidade === "Follow Line" && batalha.length <2) ? (
+                    batalha.map((item) => {
+                        return (
+                            <section className="min-card" key={item._id}>
+                                <section className="header">
+                                    <p>{item.ranking}</p>
+                                    <h5>{item.nomeCompetidor}</h5>
+                                </section>
+
+                                <section className="competidor">
+                                    <div style={{ backgroundImage: `url(${item.linkGif})`, backgroundSize: "contain", width: "100%", height: "100%", backgroundRepeat: "no-repeat", backgroundPosition: "center" }} > </div>
+                                </section>
+                                <section className="robo">
+                                    <img src={item.linkRobo} />
+                                </section>
+                                <section className="body">
+                                    <section className="content">
+                                        <p><b>Equipe:</b> <span>{item.equipe}</span></p>
+                                        <p><b>Modalidade:</b> <span>{item.modalidade}</span></p>
+                                        <p><b>Instituição:</b> <span>{item.instituicao}</span></p>
+                                    </section>
+                                </section>
+                            </section>
+                        )
+                    })) : console.log("não"))}
+            </section>
+            <section style={{ width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", margin: "20px auto 0" }}>
+                <button>batle</button>
             </section>
             <section id="continer-cards-batle">
                 {competidores.map((item) => {
                     if (item.modalidade === modalidade) {
                         return (
-                            <section className="min-card" key={item._id}>
+                            <section className="min-card" key={item._id} onClick={() => fight(item)}>
                                 <section className="header">
                                     <p>{item.ranking}</p>
                                     <h5>{item.nomeCompetidor}</h5>
