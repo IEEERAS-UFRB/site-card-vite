ca7484f90c93d2f164970835b50186b428acc832
Gabriel <gabrielmarkolinosilva14@gmail.com>
2023-10-11 10:10:31 -0300

    fix: colocando ngrok

--- src/assets/axios/config.js
+++ src/assets/axios/config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = "http://localhost:4000"
+export const baseURL = "https://f499-200-128-102-253.ngrok.io"
 
 const blogFetch = axios.create({
     baseURL: baseURL,
