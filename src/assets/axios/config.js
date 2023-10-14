81eb1f25773851f93640d13d25a115e7286317e3
Gabriel-MarcoSilva <gabrielmarkolinosilva14@gmail.com>
2023-10-14 20:57:46 -0300

    chore: add serveo

--- src/assets/axios/config.js
+++ src/assets/axios/config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = "http://localhost:4000"
+export const baseURL = "https://plecto.serveo.net"
 
 const blogFetch = axios.create({
     baseURL: baseURL,
