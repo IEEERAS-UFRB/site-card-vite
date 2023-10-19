259f2243238f0eae1dbc9b92165a92b011d60f98
Gabriel-MarcoSilva <gabrielmarkolinosilva14@gmail.com>
2023-10-19 14:40:13 -0300

    chore: aplicação de style no front do competidor

--- src/routes/Competidor/Competidor.jsx
+++ src/routes/Competidor/Competidor.jsx
@@ -41,35 +41,38 @@ const Competidor = () => {
     }
 
     return (
-        <div className="container-card container-card-comp" style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover", backgroundPosition: "center", color: "#fff", height:"90vh",
-        textShadow: "2px 0 #000, -2px 0 #000, 0 2px #000, 0 -2px #000, 1px 1px #000, -1px -1px #000, 1px -1px" }}>
-            <section style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%"}}>
+        <div className="container-card container-card-comp" style={{
+            backgroundImage: `url(${bg})`, backgroundSize: "cover", backgroundPosition: "center", color: "#fff", height: "90vh",
+            textShadow: "2px 0 #000, -2px 0 #000, 0 2px #000, 0 -2px #000, 1px 1px #000, -1px -1px #000, 1px -1px"
+        }}>
+            <section style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
                 <h3>{item.nomeCompetidor}</h3>
                 <section id="competidor" style={{ width: "auto" }}>
                     <img src={item.linkGif} alt="" id="imgComp" />
-                    <section id="info-competidor" style={{marginTop:"5px"}}>
-                        <p style={{background:"rgba(0,0,0,0.6)", padding: "3px 10px"}}><span>Instituição: </span>{item.instituicao}</p>
+                    <section id="info-competidor" style={{ marginTop: "5px" }}>
+                        <p style={{ background: "rgba(0,0,0,0.6)", padding: "3px 10px" }}><span>Instituição: </span>{item.instituicao}</p>
                         <hr />
-                        <p style={{background:"rgba(0,0,0,0.6)", padding: "3px 10px"}}><span>Ranking: </span>{item.ranking}</p>
+                        <p style={{ background: "rgba(0,0,0,0.6)", padding: "3px 10px" }}><span>Ranking: </span>{item.ranking}</p>
                         <hr />
-                        <p style={{background:"rgba(0,0,0,0.6)", padding: "3px 10px"}}><span>{item.modalidade == "Follow Line" ? "Melhor Tempo: " : "Pontuação: "}</span>{item.modalidade == "Follow Line" ? Math.floor(item.pontuacao / 100) + ":" + item.pontuacao % 100 + "s" : item.pontuacao}</p>
+                        <p style={{ background: "rgba(0,0,0,0.6)", padding: "3px 10px" }}><span>{item.modalidade == "Follow Line" ? "Melhor Tempo: " : "Pontuação: "}</span>{item.modalidade == "Follow Line" ? Math.floor(item.pontuacao / 100) + ":" + item.pontuacao % 100 + "s" : item.pontuacao}</p>
                         <hr />
-                        <p style={{background:"rgba(0,0,0,0.6)", padding: "3px 10px"}}><span>Modalidade: </span>{item.modalidade}</p>
+                        <p style={{ background: "rgba(0,0,0,0.6)", padding: "3px 10px" }}><span>Modalidade: </span>{item.modalidade}</p>
                     </section>
                 </section>
             </section>
 
             <hr />
 
-            <section id="competidor" className="equipe">
+            <section className="equipe">
 
                 <section >
-                    <h3 style={{ fontFamily: "sans-serif", fontWeight: "normal", background:"rgba(0,0,0,0.6)", padding: "3px 10px"}}> <span style={{ fontWeight: "bold" }}>Equipe:</span>  {item.equipe}</h3>
+                    <h3 style={{ fontFamily: "sans-serif", fontWeight: "normal", background: "rgba(0,0,0,0.6)", padding: "3px 10px" }}> <span style={{ fontWeight: "bold" }}>Equipe:</span>  {item.equipe}</h3>
                     {item.linkGifEquipe ? <img src={item.linkGifEquipe} /> : ""}
+                    {item.linkRobo ? <img src={item.linkRobo} /> : ""}
                 </section>
 
                 <section>
-                    <h3 style={{ fontFamily: "sans-serif", fontWeight: "normal", background:"rgba(0,0,0,0.6)", padding: "3px 10px" }}> <span style={{ fontWeight: "bold" }}>Robô:</span>  {item.nomeRobo}</h3>
+                    <h3 style={{ fontFamily: "sans-serif", fontWeight: "normal", background: "rgba(0,0,0,0.6)", padding: "3px 10px" }}> <span style={{ fontWeight: "bold" }}>Robô:</span>  {item.nomeRobo}</h3>
                     {item.linkRobo ? <img src={item.linkRobo} /> : ""}
                 </section>
 
@@ -80,9 +83,7 @@ const Competidor = () => {
                 
             </section>
 */}
-
- ranking
-            <section className="qrcodes" style={{background:"rgba(0,0,0,0.6)", padding: "3px 10px"}}>
+            <section className="qrcodes" style={{ background: "rgba(0,0,0,0.6)", padding: "3px 10px" }}>
                 <section id="competidor" onClick={() => irPara(`/https://ieeeras-ufrb.github.io/site-card-vite/${id}`)}>
                     <h3 style={{ fontFamily: "sans-serif", fontWeight: "normal" }}> <span style={{ fontWeight: "bold" }}>meu card:</span></h3>
                     <QRCode value={`/https://ieeeras-ufrb.github.io/site-card-vite/${id}`} id="qr-code-comp" />
